e1eb6f5243a707fe4248168e40db6373cfd75e63
Sherief Elnahas <sheriefelnahas5@gmail.com>
2022-10-06 14:08:30 +0200

    Move state logic to App Component & Rotate Logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,50 @@
-import './App.css';
+import "./App.css";
+
+import axios from 'axios';
 
 import React from "react";
-import Card from './Card';
+import Card from "./Card";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { cardId: "" ,imgSrc: "" };
+
+  }
+  async componentDidMount() {
+    const result = await axios("https://deckofcardsapi.com/api/deck/new/shuffle/");
+    const id = result.data.deck_id;
+    this.setState({cardId: id});
+  
+    // const result2 = await axios(`https://deckofcardsapi.com/api/deck/${id}/draw/.`);
+    // this.setState({ imgSrc: result2.data.cards[0].image });
+
+  }
+
+  async getCard() {
+    const result2 = await axios(`https://deckofcardsapi.com/api/deck/${this.state.cardId}/draw/.`);
+    this.setState({ imgSrc: result2.data.cards[0].image });
+  }
+
+
+
   render() {
+
     return (
       <div className="App">
-        <div>
-        <h1><span className="card">♦️</span> Card Dealer <span className="card">♦️</span></h1>
-        <h3><span className="card">♦️</span> A Little demo made with react <span className="card">♦️</span></h3>
+        <div className="App-card-container">
+          <div className="card container">
+          <h1> <span className="card">♦️</span> Card Dealer  <span className="card">♦️</span>
+          </h1>
+          <h3>  <span className="card">♦️</span> A Little demo made with react <span className="card">♦️</span>
+          </h3>
+          <button onClick={() => this.getCard()}>Deal me a card!</button>
+          </div>
+       
 
-        < Card />
         </div>
-
+        <Card  imgUrl={this.state.imgSrc}/>
+        <Card  imgUrl={this.state.imgSrc}/>
       </div>
     );
   }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,32 +1,18 @@
 import './Card.css';
 
 import React, { Component } from 'react'
-import axios from 'axios';
+
 
 export default class Card extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {cardId : '', imgSrc : ''};
-    }
-    async componentDidMount() {
-        const result = await axios('https://deckofcardsapi.com/api/deck/new/shuffle/');
-        const id = result.data.deck_id;
-        this.setState({cardId: id});
-        
-
-    }
-
-    async getCard() {
-        const result = await axios(`https://deckofcardsapi.com/api/deck/${this.state.cardId}/draw/.`);
-        this.setState({imgSrc: result.data.cards[0].image})
-    }
+    
 
   render() {
-    
+    let randomRotateDeg = `rotate(${Math.ceil(Math.random() * 30) * (Math.round(Math.random()) ? 1 : -1)}deg)`;
+
     return (
-      <div className='Card'>
-        <button onClick={() => this.getCard()}>Deal me a card!</button>
-        <img src={this.state.imgSrc} alt="" />
+      <div className='Card' >
+
+        <img style={{transform : randomRotateDeg}} src={this.props.imgUrl} alt="" />
 
       </div>
     )
